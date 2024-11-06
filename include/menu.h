33a1dbad8a379f136a54f906e56b695d11ad1e53
abneribeiro <eliasabner38@gmail.com>
2024-11-06 16:12:54 +0000

    add: implement menu functionality and header file

---
+++ include/menu.h
@@ -0,0 +1,15 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+#include "Biblioteca.h"
+
+
+void MenuLivros();
+void MenuListarLivros();
+void MenuRequisitantes();
+void MenuListarRequisitantes();
+void MenuRequisicoes();
+void MenuListarRequisicoes();
+void MenuAvancado();
+
+#endif // MENUS_HPP
--- main.cpp
+++ main.cpp
@@ -1,17 +1,61 @@
 #include <iostream>
-
-using namespace std;
-
+#include "include/menu.h"
 #include "include/Biblioteca.h"
 
-int main()
-{
-    cout << "Hello world! Código funcionando" << endl;
-  //  Biblioteca *BIB = new Biblioteca();
+int main() {
+   cout << "\t\tBiblioteca-ESTGV!\n";
+
+    int option;
+    do {
+        cout << "\n #----------------------------------------------------------------#";
+        cout << "\n | (1) Gerir livros                                               |";
+        cout << "\n | (2) Gerir Requisitantes                                        |";
+        cout << "\n | (3) Gerir Requisições                                          |";
+        cout << "\n | (4) Opções Avançadas                                           |";
+        cout << "\n | (5) Sair                                                       |";
+        cout << "\n | (0) Salvar e Sair                                              |";
+        cout << "\n #----------------------------------------------------------------#";
+        cout << "\nQual a sua opcao: ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                MenuLivros();
+                break;
+            case 2:
+                MenuRequisitantes();
+                break;
+            case 3:
+                MenuRequisicoes();
+                break;
+            case 4: {
+                MenuAvancado();
+                break;
+            }
+            case 5: {
+                break;
+            }
+            case 0:
+                cout << "\n\n ***** Salvando os dados *****\n";
+                cout << " ***** Saindo do programa *****\n";
+                break;
+        }
+    } while (option != 0);
+
+    return 0; 
 
 
-    // BIB->LoadFile("dados.txt");
-
-
-    return 0;
 }
+
+
+// int main()
+// {
+//     cout << "Hello world! Código funcionando" << endl;
+//   //  Biblioteca *BIB = new Biblioteca();
+
+
+//     // BIB->LoadFile("dados.txt");
+
+
+//     return 0;
+// }
