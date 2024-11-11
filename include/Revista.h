6a53895e23dc319fcf9b8d4fae27e27d29d1732f
abneribeiro <eliasabner38@gmail.com>
2024-11-11 10:47:25 +0000

    add: enhance Geral and Revista classes with constructors and info display methods

--- include/Revista.h
+++ include/Revista.h
@@ -6,8 +6,13 @@
 
 class Revista : public Geral
 {
+    int numeroEdicao;
     public:
-        Revista();
+        Revista( string titulo, string autor, int anoPublicacao, int numeroEdicao ) : Geral(titulo, autor, anoPublicacao), numeroEdicao(numeroEdicao) {};
+        void mostrarInfo() const override {
+            Geral::mostrarInfo();
+            cout << "Numero de Edicao: " << numeroEdicao << endl;
+        }
         virtual ~Revista();
 
     protected:
