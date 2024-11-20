2245377f25dde62d7f1574fe5dcd9d5585b0685d
abneribeiro <eliasabner38@gmail.com>
2024-11-20 22:07:06 +0000

    add: refactor Jornal class to improve method formatting and add getter methods for dataPublicacao and editor

--- include/Jornal.h
+++ include/Jornal.h
@@ -9,23 +9,26 @@ class Jornal : public Geral
     string dataPublicacao;
     string editor;
 
-    public:
-        Jornal();
-        Jornal( string titulo, string autor, int anoPublicacao, string dataPublicacao, string editor ) : Geral(titulo, autor, anoPublicacao), dataPublicacao(dataPublicacao), editor(editor) {};
+public:
+    Jornal();
+    Jornal(string titulo, string autor, int anoPublicacao, string dataPublicacao, string editor) : Geral(titulo, autor, anoPublicacao), dataPublicacao(dataPublicacao), editor(editor) {};
 
-        void mostrarInfo() const {
-            Geral::mostrarInfo();
-            cout << "Data de Publicacao: " << dataPublicacao << endl;
-            cout << "Editor: " << editor << endl;
-        }
-        string getCategoria() const override {
-            return "Jornal";
-        }
-        virtual ~Jornal();
+    void mostrarInfo() const
+    {
+        Geral::mostrarInfo();
+        cout << "Data de Publicacao: " << dataPublicacao << endl;
+        cout << "Editor: " << editor << endl;
+    }
+    string getCategoria() const override
+    {
+        return "Jornal";
+    }
+    string getDataPublicacao() const { return dataPublicacao; }
+    string getEditor() const { return editor; }
+    virtual ~Jornal();
 
-    protected:
-
-    private:
+protected:
+private:
 };
 
 #endif
